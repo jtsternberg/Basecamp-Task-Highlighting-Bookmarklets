16e31776f1d346f6f742baf7d155e659c7b14557
Justin Sternberg <justin@dsgnwrks.pro>
2015-09-04 22:05:51 -0400

    Make highlight bookmarklets much more robust and persistent across ajax page-loads

--- basecamp-bookmarklet.js
+++ basecamp-bookmarklet.js
@@ -1,31 +1,85 @@
-// javascript:(function(){if%20(%20!%20window.bcx%20)%20{%20return;%20}var%20email_address%20=%20window.bcx.people.models[0].get(%27email_address%27);var%20name%20=%20window.bcx.people.models[0].get(%27name%27);if%20(%20name%20&&%20email_address%20)%20{var%20behavior%20=%20%27todo_assignee_present%27;var%20$me%20=%20$(%27span[data-behavior=%27+%20behavior%20+%27]:contains(%27+%20name%20+%27)%27);if%20(%20!%20$me.length%20)%20{$me%20=%20$(%27span[data-behavior=%27+%20behavior%20+%27]:contains(%27+%20email_address%20+%27)%27);}if%20(%20$me.length%20)%20{$me.parents(%27.wrapper%27).css({%27background%27:%20%27#FFFF5C%27,%27padding%27:%20%275px%208px%205px%2030px%27,%27border-radius%27:%20%2715px%27,%27margin-left%27:%20%27-29px%27});alert(%20%27Found%20%27+%20$me.length%20+%27%20tasks%20for%20you.%27%20);}%20else%20{alert(%20%27Hey,%20looks%20like%20you\%27re%20task-free!%27%20);}}})();
+// BOOKMARKLET: javascript:(function(window,%20document,%20$,%20undefined){%27use%20strict%27;if%20(%20!%20window.bcx%20)%20{return;}if%20(%20window.BCHighlightMe%20&&%20window.BCHighlightMe.highlightMe%20)%20{window.BCHighlightMe.highlightMe();return;}var%20app%20=%20{me%20:%20window.bcx.people.models[0],initDone%20:%20false};app.highlightMe%20=%20function()%20{if%20(%20!%20app.me.get(%20%27name%27%20)%20&&%20!%20app.me.get(%20%27email_address%27%20)%20)%20{return;}var%20$me%20=%20app.getMe();if%20(%20!%20$me.length%20)%20{return%20app.init(%20%27Hey,%20looks%20like%20you\%27re%20task-free!%27%20);}$me.parents(%20%27.wrapper%27%20).addClass(%20%27BC-highlight-me%27%20);app.init(%20%27Found%20%27+%20$me.length%20+%27%20tasks%20for%20you.%27%20);};app.init%20=%20function(%20alertMsg%20)%20{if%20(%20app.initDone%20)%20{return;}alert(%20alertMsg%20);$(%20%27body%27%20).on(%20%27click%27,%20%27a%27,%20app.clickLink%20);app.addStyles();app.initDone%20=%20true;};app.addStyles%20=%20function()%20{var%20css%20=%20%27%27;css%20+=%20%27<style%20type=%22text/css%22%20media=%22screen%22>%27;css%20+=%20%27.BC-highlight-me%20{%27;css%20+=%20%27background:%20#FFFF5C;%27;css%20+=%20%27border-radius:%2015px;%27;css%20+=%20%27padding:%205px%208px%205px%2030px%20!important;%27;css%20+=%20%27margin-left:%20-29px%20!important;%27;css%20+=%20%27}%27;css%20+=%20%27</style>%27;$(%20%27head%27%20).append(%20css%20);};app.getMe%20=%20function()%20{var%20selector%20=%20function(%20search%20)%20{return%20$(%20%27span[data-behavior=todo_assignee_present]:contains(%27+%20search%20+%27)%27%20);};var%20$me%20=%20selector(%20app.me.get(%20%27name%27%20)%20);if%20(%20!%20$me.length%20)%20{$me%20=%20selector(%20app.me.get(%20%27email_address%27%20)%20);}return%20$me;};app.clickLink%20=%20function()%20{setTimeout(%20function()%20{app.highlightMe();},%201000%20);};app.highlightMe();window.BCHighlightMe%20=%20window.BCHighlightMe%20||%20app;})(window,%20document,%20jQuery);
 
+(function(window, document, $, undefined){
+	'use strict';
 
-(function(){
-	if ( ! window.bcx ) { return; }
-
-	var email_address = window.bcx.people.models[0].get('email_address');
-	var name  = window.bcx.people.models[0].get('name');
-
-	if ( name && email_address ) {
-		var behavior = 'todo_assignee_present';
-		var $me = $('span[data-behavior='+ behavior +']:contains('+ name +')');
-		if ( ! $me.length ) {
-			$me = $('span[data-behavior='+ behavior +']:contains('+ email_address +')');
-		}
-		if ( $me.length ) {
-			$me.parents('.wrapper').css({
-				'background': '#FFFF5C',
-				'padding': '5px 8px 5px 30px',
-				'border-radius': '15px',
-				'margin-left': '-29px'
-			});
-
-			alert( 'Found '+ $me.length +' tasks for you.' );
-		} else {
-			alert( 'Hey, looks like you\'re task-free!' );
-		}
+	if ( ! window.bcx ) {
+		return;
 	}
-})();
 
-// for the new people @here, I use this on a near-daily basis for seeing the tasks assigned to me more easily in BC: http://dsgnwrks.pro/plugins-and-scripts/bookmarklet-to-highlight-tasks-assigned-to-me-in-new-basecamp/
+	if ( window.BCHighlightMe && window.BCHighlightMe.highlightMe ) {
+		window.BCHighlightMe.highlightMe();
+		return;
+	}
+
+	var app = {
+		me : window.bcx.people.models[0],
+		initDone : false
+	};
+
+	app.highlightMe = function() {
+		if ( ! app.me.get( 'name' ) && ! app.me.get( 'email_address' ) ) {
+			return;
+		}
+
+		var $me = app.getMe();
+
+		if ( ! $me.length ) {
+			return app.init( 'Hey, looks like you\'re task-free!' );
+		}
+
+		$me.parents( '.wrapper' ).addClass( 'BC-highlight-me' );
+		app.init( 'Found '+ $me.length +' tasks for you.' );
+	};
+
+	app.init = function( alertMsg ) {
+		if ( app.initDone ) {
+			return;
+		}
+
+		alert( alertMsg );
+
+		$( 'body' ).on( 'click', 'a', app.clickLink );
+		app.addStyles();
+
+		app.initDone = true;
+	};
+
+	app.addStyles = function() {
+		var css = '';
+		css += '<style type="text/css" media="screen">';
+			css += '.BC-highlight-me {';
+				css += 'background: #FFFF5C;';
+				css += 'border-radius: 15px;';
+				css += 'padding: 5px 8px 5px 30px !important;';
+				css += 'margin-left: -29px !important;';
+			css += '}';
+		css += '</style>';
+		$( 'head' ).append( css );
+	};
+
+	app.getMe = function() {
+		var selector = function( search ) {
+			return $( 'span[data-behavior=todo_assignee_present]:contains('+ search +')' );
+		};
+
+		var $me = selector( app.me.get( 'name' ) );
+
+		if ( ! $me.length ) {
+			$me = selector( app.me.get( 'email_address' ) );
+		}
+
+		return $me;
+	};
+
+	app.clickLink = function() {
+		setTimeout( function() {
+			app.highlightMe();
+		}, 1000 );
+	};
+
+	app.highlightMe();
+
+	window.BCHighlightMe = window.BCHighlightMe || app;
+
+})(window, document, jQuery);
--- basecamp-bookmarklet2.js
+++ basecamp-bookmarklet2.js
@@ -1,34 +1,80 @@
-// javascript:(function($,%20undefined){var%20search_name%20=%20prompt(%20%27Search%20Name:%27%20);if%20(search_name%20!==%20null)%20{jQuery.expr[%27:%27].Contains%20=%20function(a,%20i,%20m)%20{return%20jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase())%20>=%200;};var%20behavior%20=%20%27todo_assignee_present%27;var%20$search_name%20=%20$(%27span[data-behavior=%27+%20behavior%20+%27]:Contains(%27+%20search_name%20+%27)%27);if%20(%20!%20$search_name.length%20)%20{$search_name%20=%20$(%27span[data-behavior=%27+%20behavior%20+%27]:Contains(%27+%20search_name%20+%27)%27);}if%20(%20!%20$search_name.length%20)%20{return%20false;}$(%27span[data-behavior]%27).parents(%27.wrapper%27).css({%27background%27:%20%27none%27,%27padding%27:%20%270%27,%27margin-left%27:%20%270%27});$search_name.parents(%27.wrapper%27).css({%27background%27:%20%27#A4FF5C%27,%27padding%27:%20%275px%208px%205px%2030px%27,%27border-radius%27:%20%2715px%27,%27margin-left%27:%20%27-29px%27});alert(%20$search_name.parents(%27.wrapper%27).length%20+%20%27%20tasks%20found%20for%20%27+%20search_name%20+%20%27.%27%20);}})(jQuery);
+// BOOKMARKLET: javascript:(function(window,%20document,%20$,%20undefined){%27use%20strict%27;if%20(%20!%20window.bcx%20)%20{return;}if%20(%20window.BCHighlightUser%20&&%20window.BCHighlightUser.init%20)%20{window.BCHighlightUser.init();return;}var%20app%20=%20{search_name%20:%20%27%27,initDone%20:%20false};$.expr[%27:%27].Contains%20=%20function(a,%20i,%20m)%20{return%20$(a).text().toUpperCase().indexOf(m[3].toUpperCase())%20>=%200;};app.init%20=%20function()%20{app.search_name%20=%20prompt(%20%27Search%20Name:%27%20);var%20$search%20=%20app.highlightUser();var%20number%20=%20false%20!==%20$search%20&&%20$search.length%20?%20$search.length%20:%20%27No%27;alert(%20number%20+%20%27%20tasks%20found%20for%20%27+%20app.search_name%20+%20%27.%27%20);if%20(%20!%20app.initDone%20)%20{$(%20%27body%27%20).on(%20%27click%27,%20%27a%27,%20app.clickLink%20);app.addStyles();}app.initDone%20=%20true;};app.addStyles%20=%20function()%20{var%20css%20=%20%27%27;css%20+=%20%27<style%20type=%22text/css%22%20media=%22screen%22>%27;css%20+=%20%27.BC-user-highlight%20{%27;css%20+=%20%27background:%20#A4FF5C;%27;css%20+=%20%27border-radius:%2015px;%27;css%20+=%20%27padding:%205px%208px%205px%2030px%20!important;%27;css%20+=%20%27margin-left:%20-29px%20!important;%27;css%20+=%20%27}%27;css%20+=%20%27</style>%27;$(%20%27head%27%20).append(%20css%20);};app.highlightUser%20=%20function()%20{if%20(%20!%20app.search_name%20)%20{return;}var%20$search%20=%20$(%20%27span[data-behavior=todo_assignee_present]:Contains(%27+%20app.search_name%20+%27)%27%20);if%20(%20!%20$search.length%20)%20{return%20false;}$(%20%27.BC-user-highlight%27%20).removeClass(%20%27BC-user-highlight%27%20);return%20$search.parents(%20%27.wrapper%27%20).addClass(%20%27BC-user-highlight%27%20);};app.clickLink%20=%20function()%20{setTimeout(%20function()%20{app.highlightUser();},%201000%20);};app.init();window.BCHighlightUser%20=%20window.BCHighlightUser%20||%20app;})(window,%20document,%20jQuery);
 
-(function($, undefined){
+(function(window, document, $, undefined){
+	'use strict';
 
-	var search_name = prompt( 'Search Name:' );
-
-	if (search_name !== null) {
-		jQuery.expr[':'].Contains = function(a, i, m) {
-			return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
-		};
-		var behavior = 'todo_assignee_present';
-		var $search_name = $('span[data-behavior='+ behavior +']:Contains('+ search_name +')');
-		if ( ! $search_name.length ) {
-			$search_name = $('span[data-behavior='+ behavior +']:Contains('+ search_name +')');
-		}
-		if ( ! $search_name.length ) {
-			return false;
-		}
-		$('span[data-behavior]').parents('.wrapper').css({
-			'background': 'none',
-			'padding': '0',
-			'margin-left': '0'
-		});
-		$search_name.parents('.wrapper').css({
-			'background': '#A4FF5C',
-			'padding': '5px 8px 5px 30px',
-			'border-radius': '15px',
-			'margin-left': '-29px'
-		});
-
-		alert( $search_name.parents('.wrapper').length + ' tasks found for '+ search_name + '.' );
+	if ( ! window.bcx ) {
+		return;
 	}
 
-})(jQuery);
+	if ( window.BCHighlightUser && window.BCHighlightUser.init ) {
+		window.BCHighlightUser.init();
+		return;
+	}
+
+	var app = {
+		search_name : '',
+		initDone : false
+	};
+
+	$.expr[':'].Contains = function(a, i, m) {
+		return $(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+	};
+
+	app.init = function() {
+
+		app.search_name = prompt( 'Search Name:' );
+
+		var $search = app.highlightUser();
+
+		var number = false !== $search && $search.length ? $search.length : 'No';
+		alert( number + ' tasks found for '+ app.search_name + '.' );
+
+		if ( ! app.initDone ) {
+			$( 'body' ).on( 'click', 'a', app.clickLink );
+			app.addStyles();
+		}
+
+		app.initDone = true;
+	};
+
+	app.addStyles = function() {
+		var css = '';
+		css += '<style type="text/css" media="screen">';
+			css += '.BC-user-highlight {';
+				css += 'background: #A4FF5C;';
+				css += 'border-radius: 15px;';
+				css += 'padding: 5px 8px 5px 30px !important;';
+				css += 'margin-left: -29px !important;';
+			css += '}';
+		css += '</style>';
+		$( 'head' ).append( css );
+	};
+
+	app.highlightUser = function() {
+		if ( ! app.search_name ) {
+			return;
+		}
+
+		var $search = $( 'span[data-behavior=todo_assignee_present]:Contains('+ app.search_name +')' );
+
+		if ( ! $search.length ) {
+			return false;
+		}
+
+		$( '.BC-user-highlight' ).removeClass( 'BC-user-highlight' );
+
+		return $search.parents( '.wrapper' ).addClass( 'BC-user-highlight' );
+	};
+
+	app.clickLink = function() {
+		setTimeout( function() {
+			app.highlightUser();
+		}, 1000 );
+	};
+
+	app.init();
+
+	window.BCHighlightUser = window.BCHighlightUser || app;
+
+})(window, document, jQuery);
